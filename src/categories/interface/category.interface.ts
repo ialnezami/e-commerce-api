9284e77262254cc8d2940c633dcfecbcc570f78c
ialnezami <ibrahim.alnezami@gmail.com>
2024-09-07 21:24:14 +0200

    chore: schema category

--- src/categories/interface/category.interface.ts
+++ src/categories/interface/category.interface.ts
@@ -1,5 +1,26 @@
-import { Schema } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Document } from 'mongoose';
 
-export const Category = new Schema({
-  name: { type: String, required: true },
-});
+@Schema({
+  collection: 'categories',
+  timestamps: true, // Ajoute les champs createdAt et updatedAt automatiquement
+})
+export class Category {
+  @ApiProperty({
+    type: String,
+    description: 'The unique identifier of the category',
+  })
+  @Prop({ required: true })
+  id: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'The name of the category',
+  })
+  @Prop({ required: true })
+  name: string;
+}
+
+export const CategorySchema = SchemaFactory.createForClass(Category);
+export type ICategory = Category & Document;
