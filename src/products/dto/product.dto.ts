1114eb1b5fd5835bbc28047a5f18cd666ca26e31
ialnezami <ibrahim.alnezami@gmail.com>
2024-09-07 21:38:21 +0200

    feat: Added imports, defined schema with timestamps, created Product class

--- src/products/dto/product.dto.ts
+++ src/products/dto/product.dto.ts
@@ -1,8 +1,54 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Document } from 'mongoose';
+
+@Schema({
+  collection: 'products',
+  timestamps: true, // Ajoute les champs createdAt et updatedAt automatiquement
+})
 export class Product {
+  @ApiProperty({
+    type: Number,
+    description: 'Unique identifier for the product',
+  })
+  @Prop({ required: true })
   id: number;
+
+  @ApiProperty({
+    type: String,
+    description: 'Name of the product',
+  })
+  @Prop({ required: true })
   name: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Detailed description of the product',
+  })
+  @Prop({ required: true })
   description: string;
+
+  @ApiProperty({
+    type: Number,
+    description: 'Price of the product',
+  })
+  @Prop({ required: true })
   price: number;
+
+  @ApiProperty({
+    type: Number,
+    description: 'Stock availability of the product',
+  })
+  @Prop({ required: true })
   stock: number;
+
+  @ApiProperty({
+    type: Number,
+    description: 'Identifier of the category the product belongs to',
+  })
+  @Prop({ required: true })
   categoryId: number;
 }
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
+export type IProduct = Product & Document;
