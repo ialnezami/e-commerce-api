1287944eba36d0b03b1a94d092499610c4422641
ialnezami <ibrahim.alnezami@gmail.com>
2024-09-08 11:25:19 +0200

    fix: add mongooose model

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductSchema } from './dto/product.dto';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService],
 })
 export class ProductsModule {}
